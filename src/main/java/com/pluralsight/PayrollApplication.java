360c874651a1ed1cddd423d0504eb8eeca71957c
Mamii868 <marcmotenii@gmail.com>
2026-04-20 15:50:31 -0500

    Add create employee method

--- src/main/java/com/pluralsight/PayrollApplication.java
+++ src/main/java/com/pluralsight/PayrollApplication.java
@@ -4,4 +4,14 @@ public class PayrollApplication {
     public static void main(String[] args) {
 
     }
+
+    public static Employee createEmployee(String employeeData) {
+        String[] splitEmployeeData = employeeData.split("\\|");
+        int employeeId = Integer.parseInt(splitEmployeeData[0]);
+        String employeeName = splitEmployeeData[1];
+        double employeeHoursWorked = Double.parseDouble(splitEmployeeData[2]);
+        double employeePayRate = Double.parseDouble(splitEmployeeData[3]);
+
+        return new Employee(employeeId, employeeName, employeeHoursWorked, employeePayRate);
+    }
 }
