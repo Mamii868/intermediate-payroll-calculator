7c232e7b639de1298d22cb29793129f449d854f1
Mamii868 <marcmotenii@gmail.com>
2026-04-21 10:38:49 -0500

    Add Printing employees

--- src/main/java/com/pluralsight/PayrollApplication.java
+++ src/main/java/com/pluralsight/PayrollApplication.java
@@ -1,8 +1,31 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class PayrollApplication {
     public static void main(String[] args) {
+        try {
+            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
 
+//            Cut the first line since it does not have data
+            bufferedReader.readLine();
+
+            String currentLine;
+
+//            Print out each employee in the list with correct formatting
+            while ((currentLine = bufferedReader.readLine()) != null) {
+
+                Employee newEmployee = createEmployee(currentLine);
+
+                System.out.printf("ID: %s, Name: %s, Gross Pay: $%.2f\n", newEmployee.getId(), newEmployee.getName(), newEmployee.getGrossPay());
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error!");
+            e.printStackTrace();
+        }
     }
 
     public static Employee createEmployee(String employeeData) {
